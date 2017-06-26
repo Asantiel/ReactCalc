eeafe6fd91304517e713faa1012a91e1f736488a
Asantiel <deadyork@gmail.com>
2017-06-26 11:27:52 +0400

    First commit

---
+++ ReactCalc/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactCalc
+{
+    class Calc
+    {
+        public Calc(int x)
+        {
+            X = x;
+        }
+
+        public int X { get; private set; }
+
+        public static int Y { get; set;}
+
+        public string H
+        {
+            get
+            {
+                var f = X + Y;
+                return string.Format("{0}", f);
+            }
+        }
+    }
+}
