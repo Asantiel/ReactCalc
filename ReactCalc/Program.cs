79af383bf3173a661964c735a18184781d47eb55
Asantiel <deadyork@gmail.com>
2017-06-26 12:55:28 +0400

    Реально рабочая версия

--- ReactCalc/Program.cs
+++ ReactCalc/Program.cs
@@ -11,8 +11,52 @@ namespace ReactCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int x = 0;
+            int y = 0;
+            var calc = new Calc();
+
+            if (args.Length == 2)
+            {
+                x = ToInt(args[0], 83);
+                y = ToInt(args[1], 70);
+            }
+            else
+            {
+                #region Ввод данных
+                Console.WriteLine("Введите Х");
+                var strx = Console.ReadLine();
+
+                if (!int.TryParse(strx, out x))
+                {
+                    x = 100;
+                }
+
+                Console.WriteLine("Введите Y");
+                var stry = Console.ReadLine();
+
+                if (!int.TryParse(stry, out y))
+                {
+                    y = 100;
+                }
+                #endregion
+            }
+
+            var result = calc.Sum(x, y);
+
+            Console.WriteLine(string.Format("Сумма = {0}", result));
             Console.ReadKey();
+        }
+
+
+        private static int ToInt(string arg, int i = 100)
+        {
+            int x;
+            if (!int.TryParse(arg, out x))
+            {
+                x = i;
+            }
+
+            return x;
         }
     }
 }
